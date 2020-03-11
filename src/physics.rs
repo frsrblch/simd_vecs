c902c42942c501089c30c5b168a1d2a5bd82bdcc
Fraser Balch <farseer.ulwithy@gmail.com>
2020-03-10 19:21:03 -0600

    refactoring

--- src/physics.rs
+++ src/physics.rs
@@ -1,8 +1,6 @@
 use std::marker::PhantomData;
 use std::ops::*;
 use crate::ops::*;
-use crate::vecs::*;
-use crate::units::*;
 
 #[derive(Debug, Default, Clone, PartialEq)]
 pub struct Typed<VEC, UNIT> {
@@ -53,40 +51,47 @@ impl<'a, VEC1, UNIT1, VEC2, UNIT2> Mul<&'a Typed<VEC2, UNIT2>> for &'a Typed<VEC
     }
 }
 
-#[test]
-fn add_assign_typed_vecs() {
-    let mut position: Typed<Vec2<f32>, Meters> = Typed {
-        vec: Vec2 {
-            x: vec![1.0, 2.0].into(),
-            y: vec![3.0, 5.0].into(),
-        },
-        marker: PhantomData,
-    };
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::vecs::*;
+    use crate::units::*;
 
-    let velocity: Typed<Vec2<f32>, MetersPerSecond> = Typed {
-        vec: Vec2 {
-            x: vec![2.0, 3.0].into(),
-            y: vec![5.0, 7.0].into(),
-        },
-        marker: PhantomData,
-    };
+    #[test]
+    fn add_assign_typed_vecs() {
+        let mut position: Typed<Vec2<f32>, Meters> = Typed {
+            vec: Vec2 {
+                x: vec![1.0, 2.0].into(),
+                y: vec![3.0, 5.0].into(),
+            },
+            marker: PhantomData,
+        };
 
-    let time: Typed<Vec1<f32>, Seconds> = Typed {
-        vec: Vec1 {
-            values: vec![1.0, 2.0].into(),
-        },
-        marker: PhantomData,
-    };
+        let velocity: Typed<Vec2<f32>, MetersPerSecond> = Typed {
+            vec: Vec2 {
+                x: vec![2.0, 3.0].into(),
+                y: vec![5.0, 7.0].into(),
+            },
+            marker: PhantomData,
+        };
 
-    let expected: Typed<Vec2<f32>, Meters> = Typed {
-        vec: Vec2 {
-            x: vec![3.0, 8.0].into(),
-            y: vec![8.0, 19.0].into(),
-        },
-        marker: PhantomData,
-    };
+        let time: Typed<Vec1<f32>, Seconds> = Typed {
+            vec: Vec1 {
+                values: vec![1.0, 2.0].into(),
+            },
+            marker: PhantomData,
+        };
 
-    position += &velocity * &time;
+        let expected: Typed<Vec2<f32>, Meters> = Typed {
+            vec: Vec2 {
+                x: vec![3.0, 8.0].into(),
+                y: vec![8.0, 19.0].into(),
+            },
+            marker: PhantomData,
+        };
 
-    assert_eq!(expected, position);
+        position += &velocity * &time;
+
+        assert_eq!(expected, position);
+    }
 }
